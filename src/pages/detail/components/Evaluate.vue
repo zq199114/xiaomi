722b50a66544634fc3e01a76fc417b09db34e5a2
unknown <ndsl_x@qq.com>
2018-05-29 17:55:10 +0800

    detail-content

---
+++ src/pages/detail/components/Evaluate.vue
@@ -0,0 +1,14 @@
+<!-- 展示模板 -->
+<template>
+
+</template>
+
+<script>
+  export default {
+    name: 'Home'
+  }
+</script>
+<!-- 样式代码 -->
+<style scoped lang="stylus" type="text/stylus">
+
+</style>
